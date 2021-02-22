499c3866d6cd13f372fecedef0a840dd57577860
qompu <mtamayo11@hawkmail.hccfl.edu>
2021-02-22 17:09:18 -0500

    Added header comments

--- src/main/java/coffee123/searchengine3/UserInterface.java
+++ src/main/java/coffee123/searchengine3/UserInterface.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Search Engine.
+ * COP 2805C (Java II) Project
+ * Team: Coffee123: Adriel Lopez, Manuel Tamayo
  */
 package coffee123.searchengine3;
 
