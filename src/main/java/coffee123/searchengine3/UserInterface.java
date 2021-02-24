5bdb3b167e21ffe6dc239d2fee0d0267ba691228
qompu <mtamayo11@hawkmail.hccfl.edu>
2021-02-23 19:49:21 -0500

    Proj-3: GUI for future search engine. AWT was chosen after a UI was built by Adriel using the NetBeans IDE and the team decided that AWT would be easier to troubleshoot. We may use Swing in future versions.

--- src/main/java/coffee123/searchengine3/UserInterface.java
+++ src/main/java/coffee123/searchengine3/UserInterface.java
@@ -1,6 +1,10 @@
 /*
  * Search Engine. 
- *GUI for future search engine 
+ * GUI for future search engine. 
+ * AWT was chosen after a UI was built by Adriel using the Netbeans IDE 
+ * and the team decided that AWT would be easier to troubleshoot. 
+ * We may use Swing in future versions.
+ * GUI for future search engine 
  * COP 2805C (Java II) Project
  * Team: Coffee123: Adriel Lopez, Manuel Tamayo
  */
